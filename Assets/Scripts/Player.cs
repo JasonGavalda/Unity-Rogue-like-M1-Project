f1ec6255ba35b01cac47b8cb850dc9673b635673
schoenbm <maxime.schoenberger@edu.esiee.fr>
2020-12-08 14:22:20 +0100

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,8 +17,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         Vector3 direction = new Vector3( Input.GetAxis( "Horizontal"), Input.GetAxis( "Vertical"), 0);
-        this.g
-
+        this.transform.localScale = new Vector3(direction.x * playerStats.size / Mathf.Abs(direction.x), playerStats.size, playerStats.size);
 
         this.transform.position += direction * playerStats.speed * Time.deltaTime;
         Debug.Log(direction);
