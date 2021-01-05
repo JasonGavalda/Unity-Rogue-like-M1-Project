ef4136be701af290a08acbe9c275d7f72ba8a35c
VincentAcien <VincentAcien@users.noreply.github.com>
2021-01-05 17:26:14 +0100

    AttackCAC

---
+++ Assets/Scripts/AttackCAC.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearPunch : Attack
+{
+    //private bool playerInRange = false;
+    public GameObject center;
+    public float radius;
+    protected Collider2D[] colliders;
+    public bool isPlayer;
+    [SerializeField]
+    private string[] layers;
+    private int layersToCheck;
+
+    private void Start()
+    {
+        layersToCheck = LayerMask.GetMask(layers);
+    }
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player")) ;
+    //    {
+    //        playerInRange = true;
+    //    }
+    //}
+    //void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player")) ;
+    //    {
+    //        playerInRange = false;
+    //    }
+    //}
+    protected override void useAttack()
+    {
+        colliders = Physics2D.OverlapCircleAll(center.transform.position,radius,layersToCheck);
+        foreach (Collider2D entity in colliders)
+        {
+            if (entity.gameObject != this.gameObject)
+                entity.gameObject.GetComponent<Entity>().TakeDamage(damage);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(center.transform.position,radius);
+    }
+}
+
