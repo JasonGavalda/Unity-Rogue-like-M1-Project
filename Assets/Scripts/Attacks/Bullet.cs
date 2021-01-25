2fdd4f396bea0c70c1f7c80a223040e91f23f123
schoenbm <maxime.schoenberger@edu.esiee.fr>
2021-01-25 15:38:36 +0100

    squirell

--- Assets/Scripts/Attacks/Bullet.cs
+++ Assets/Scripts/Attacks/Bullet.cs
@@ -4,44 +4,73 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private BoxCollider2D box;
+
     public float lifeTime;
-    private float timeLived = 0f;
+    private float timeLived;
     public float explosionRadius;
     private int damage;
     private int layers;
     private Collider2D[] colliders;
+    private Vector2 vel;
+    public string obstacleLayer;
+
+    void Start()
+    {
+        timeLived = 0f;
+        box = this.gameObject.GetComponent<BoxCollider2D>();
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+        //layers += LayerMask.NameToLayer(obstacleLayer);
+    }
 
     // Update is called once per frame
     public void launch(Vector2 direction, float speed, int pDamage, int intelligence, int layerstocheck)
     {
         if (rb == null)
+        {
+            Debug.Log("rb null");
             rb = this.GetComponent<Rigidbody2D>();
-        rb.velocity = direction * speed;
+        }
+        vel.x = 1; // direction.x * speed;
+        vel.y = 0; // direction.y * speed;
+        Debug.Log(vel);
         damage = pDamage + intelligence;
         layers = layerstocheck;
+        Debug.Log("Launched");
     }
 
+    /*
     private void OnCollisionEnter2D(Collision2D collision)
     {
         colliders = Physics2D.OverlapCircleAll(this.gameObject.transform.position, explosionRadius, layers);
         foreach (Collider2D entity in colliders)
         {
-            if (entity.gameObject != this.gameObject)
-            {
-                Entity ent = entity.gameObject.GetComponent<Entity>();
-                if (ent != null)
-                    ent.TakeDamage(damage);
+            Debug.Log("boum");
+            Entity ent = entity.gameObject.GetComponent<Entity>();
+            if (ent != null) {
+                ent.TakeDamage(damage);
+                Destroy(this.gameObject);
             }
+
         }
+        Debug.Log("test");
     }
+    */
 
     private void Update()
     {
+        if (rb.velocity.magnitude < vel.magnitude)
+        {
+            Debug.Log("inferiorspeed");
+            rb.velocity = vel;
+        }
+        Debug.Log(vel);
+
         timeLived += Time.deltaTime;
         if(timeLived > lifeTime)
         {
-            Destroy(this);
+            Destroy(this.gameObject);
         }
     }
 
