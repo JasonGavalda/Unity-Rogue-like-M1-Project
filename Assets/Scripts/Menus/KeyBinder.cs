c68d31abefa9dfbe765ff5cbd5f5a9569dab931c
gavaldaj <jason.gavalda@edu.esiee.fr>
2021-01-26 09:13:16 +0100

    Affichage des touches dans le menu + un script pour le futur menu des contrôles (en chantier)

---
+++ Assets/Scripts/Menus/KeyBinder.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyBinder : MonoBehaviour
+{
+    private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+
+    public Text up, left, down, right, attack, special;
+
+    private GameObject currentKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keys.Add("Up", KeyCode.W);
+        keys.Add("Down", KeyCode.S);
+        keys.Add("Left", KeyCode.A);
+        keys.Add("Right", KeyCode.D);
+        keys.Add("Fire1", KeyCode.Mouse0);
+        keys.Add("Fire2", KeyCode.Mouse1);
+
+        up.text = keys["Up"].ToString();
+        left.text = keys["Left"].ToString();
+        down.text = keys["Down"].ToString();
+        right.text = keys["Right"].ToString();
+        attack.text = keys["Fire1"].ToString();
+        special.text = keys["Fire2"].ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        if (currentKey != null)
+        {
+            Event e = Event.current;
+            if(e.isKey)
+            {
+                keys[currentKey.name] = e.keyCode;
+                currentKey.transform.GetChild(0).GetComponent<Text>().text = e.keyCode.ToString();
+                currentKey = null;
+            }
+        }
+    }
+
+    public void ChangeKey(GameObject clicked)
+    {
+        currentKey = clicked;
+    }
+}
