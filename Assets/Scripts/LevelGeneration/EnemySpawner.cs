3a2ac16df4ae638b91d08a1c34827cfb5c63dc0f
gavaldaj <jason.gavalda@edu.esiee.fr>
2021-03-09 18:29:25 +0100

    Maquette de génération procédurale d'ennemis (avec script et scène de test)

--- Assets/Scripts/LevelGeneration/EnemySpawner.cs
+++ Assets/Scripts/LevelGeneration/EnemySpawner.cs
@@ -5,29 +5,26 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject[] enemies;
-    public Transform[] spawnPoints;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        int EnemySpawn = Random.Range(0, 2);
+        if (EnemySpawn == 1)
+        {
+            Invoke("Spawn", 0.5f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        int randNumberEnemies = Random.Range(0, 4);
-        for(int i = 0; i<randNumberEnemies; i++)
-        {
-            Invoke("Spawn", 0.1f);
-        }
     }
 
     void Spawn()
     {
         int randEnemy = Random.Range(0, enemies.Length);
-        int randSpawnPoint = Random.Range(0, spawnPoints.Length);
 
-        Instantiate(enemies[randEnemy], spawnPoints[randSpawnPoint].position, transform.rotation);
+        Instantiate(enemies[randEnemy], transform.position, transform.rotation);
     }
 }
