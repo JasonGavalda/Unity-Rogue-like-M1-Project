06d08684174a48f422cdd699b85dba59376b0c08
Axel-Allio <axel.allio1@gmail.com>
2021-03-16 18:48:52 +0100

    Multiplayer

---
+++ Assets/Scripts/Multiplayer/GameSetupController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Photon.Pun;
+
+public class GameSetupController : MonoBehaviour
+{
+    void Start()
+    {
+        CreatePlayer();
+    }
+
+    private void CreatePlayer()
+    {
+        Debug.Log("Creating player");
+        PhotonNetwork.Instantiate(Path.Combine("Prefab","Entities", "Pink_Monster"), Vector3.zero, Quaternion.identity);
+    }
+}
