c817099ba057da65368a9d1572914c80a7168ae8
gavaldaj <jason.gavalda@edu.esiee.fr>
2021-03-30 17:25:11 +0200

    Correction prefabs boss rooms

--- Assets/Scripts/LevelGeneration/RoomTemplates.cs
+++ Assets/Scripts/LevelGeneration/RoomTemplates.cs
@@ -9,6 +9,7 @@ public class RoomTemplates : MonoBehaviour
     public GameObject[] topRooms;
     public GameObject[] leftRooms;
     public GameObject[] rightRooms;
+    public GameObject[] bossRooms;
 
     public List<GameObject> rooms;
 
@@ -23,12 +24,38 @@ public class RoomTemplates : MonoBehaviour
     {
         if(waitTime <= 0 && spawnedBoss == false)
         {
-            for (int i = 0; i < rooms.Count; i++)
+            for (int i = rooms.Count-1; i >=0; i--)
             {
                 if(i == rooms.Count-1)
                 {
-                    // Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
-                    spawnedBoss = true;
+                    if (rooms[i].name == "B(Clone)" || rooms[i].name == "B2(Clone)")
+                    {
+                        Instantiate(bossRooms[0], rooms[i].transform.position, Quaternion.identity);
+                        Destroy(rooms[i]);
+                        // Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
+                        spawnedBoss = true;
+                    }
+                    else if (rooms[i].name == "L(Clone)" || rooms[i].name == "L2(Clone)")
+                    {
+                        Instantiate(bossRooms[1], rooms[i].transform.position, Quaternion.identity);
+                        Destroy(rooms[i]);
+                        // Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
+                        spawnedBoss = true;
+                    }
+                    else if (rooms[i].name == "R(Clone)" || rooms[i].name == "R2(Clone)")
+                    {
+                        Instantiate(bossRooms[2], rooms[i].transform.position, Quaternion.identity);
+                        Destroy(rooms[i]);
+                        // Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
+                        spawnedBoss = true;
+                    }
+                    else if (rooms[i].name == "T(Clone)" || rooms[i].name == "T2(Clone)")
+                    {
+                        Instantiate(bossRooms[3], rooms[i].transform.position, Quaternion.identity);
+                        Destroy(rooms[i]);
+                        // Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
+                        spawnedBoss = true;
+                    }
                 }
             }
         }
