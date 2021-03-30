682f5009f9d4a29b3660a5f873c6e9a9f20f44e3
Axel-Allio <axel.allio1@gmail.com>
2021-03-30 10:40:36 +0200

    multi

--- Assets/Scripts/Entity/Player.cs
+++ Assets/Scripts/Entity/Player.cs
@@ -1,21 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Player : Entity
 {
     public Attack basicAttack;
     public Attack specialAttack;
 
+    Rigidbody2D RB;
+    PhotonView PV;
+
+    private void Awake()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        PV = GetComponent<PhotonView>();
+    }
+
+    private void Start()
+    {
+        if (!PV.IsMine)
+        {
+            Destroy(GetComponentInChildren<Camera>().gameObject);
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (!PV.IsMine)
+        {
+            return;
+        }
         Vector3 direction = new Vector3( Input.GetAxis( "Horizontal"), Input.GetAxis( "Vertical"), 0);
 
         if(direction.x != 0)
             this.transform.localScale = new Vector3(direction.x * stats.size / Mathf.Abs(direction.x), stats.size, stats.size);
 
-        rb.MovePosition( rb.transform.position + direction * stats.speed * Time.deltaTime);
+        RB.MovePosition( RB.transform.position + direction * stats.speed * Time.deltaTime);
     }
 
     private void Update()
